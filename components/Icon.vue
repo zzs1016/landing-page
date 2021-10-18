b6a7ef5fab3110b1dff489d1b9f24c031083a7b3
zhaozisen <lh@ZzsdeMacBook-Pro.local>
2021-10-18 16:15:14 +0800

    init

---
+++ components/Icon.vue
@@ -0,0 +1,32 @@
+<template>
+  <svg class="svg-icon" aria-hidden="true">
+    <use :xlink:href="iconName" />
+  </svg>
+</template>
+
+<script>
+export default {
+  name: 'Icon',
+  props: {
+    icon: {
+      type: String,
+      required: true
+    }
+  },
+  computed: {
+    iconName() {
+      return `#${this.icon}`;
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.svg-icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="home">首页
+    <icon icon="icon-fankui" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
